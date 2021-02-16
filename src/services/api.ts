e520620273495d3b15eb7127cf0ab932133da58b
Samuel Terra <samuelterra22@hotmail.com>
2021-02-16 09:29:06 -0300

    Add 401 authentication error tratative

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,21 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
+
+api.interceptors.request.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  error => {
+    // Return any error which is not due to authentication back to the calling service
+    if (error.response.status !== 401) {
+      return new Promise((resolve, reject) => {
+        reject(error);
+      });
+    }
+  },
+);
 
 export default api;
