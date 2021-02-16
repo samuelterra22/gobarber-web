9a638f2130e441d78dd966a647d7c139785977a7
Samuel Terra <samuelterra22@hotmail.com>
2021-02-16 09:37:04 -0300

    Comment axios interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,21 +1,21 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
-api.interceptors.request.use(
-  (response: AxiosResponse) => {
-    return response;
-  },
-  error => {
-    // Return any error which is not due to authentication back to the calling service
-    if (error.response.status !== 401) {
-      return new Promise((resolve, reject) => {
-        reject(error);
-      });
-    }
-  },
-);
+// api.interceptors.request.use(
+//   (response: AxiosResponse) => {
+//     return response;
+//   },
+//   error => {
+//     // Return any error which is not due to authentication back to the calling service
+//     if (error.response.status !== 401) {
+//       return new Promise((resolve, reject) => {
+//         reject(error);
+//       });
+//     }
+//   },
+// );
 
 export default api;
